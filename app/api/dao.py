cd80d33db3deac62753cd93c52dace0dc3e59b08
masleneck <vlad.rybchin@gmail.com>
2025-03-09 03:57:27 +0300

    убрал кешик(чтобы не был на гите)

--- app/api/dao.py
+++ app/api/dao.py
@@ -7,20 +7,7 @@ from sqlalchemy.orm import joinedload, selectinload
 
 from app.schemas.users import UserBase
 from app.dao.base import BaseDAO
-from app.models import User, Profile, Workout, WorkoutInfo, Meal, MealFoodItem, FoodItem, TestQuestion, TestResult
-
-
-class UserDAO(BaseDAO):
-    model = User
-
-class ProfileDAO(BaseDAO):
-    model = Profile
-
-class WorkoutDAO(BaseDAO):
-    model = Workout
-
-class WorkoutInfoDAO(BaseDAO):
-    model = WorkoutInfo
+from app.models import Meal, MealFoodItem, FoodItem
 
 class MealDAO(BaseDAO):
     model = Meal
@@ -30,9 +17,3 @@ class MealFoodItemDAO(BaseDAO):
 
 class FoodItemDAO(BaseDAO):
     model = FoodItem
-
-class TestQuestionDAO(BaseDAO):
-    model = TestQuestion
-
-class TestResultDAO(BaseDAO):
-    model = TestResult
