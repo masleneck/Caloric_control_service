156e56fc02d32432d17fd4c5edfe8e156bce39af
masleneck <vlad.rybchin@gmail.com>
2025-03-22 23:37:10 +0300

    update dao_dep

--- app/dependencies/dao_dep.py
+++ app/dependencies/dao_dep.py
@@ -1,33 +1,14 @@
-from typing import AsyncGenerator
+from typing import Annotated
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.data.db import async_session_maker
 
 
-async def get_session_with_commit() -> AsyncGenerator[AsyncSession, None]:
-    '''
-    Асинхронная сессия с автоматическим коммитом.
-    get_session_with_commit создает сессию и автоматически выполняет коммит после завершения.
-    Все изменения выполняются в одной транзакции.
-    Если возникает ошибка, транзакция откатывается автоматически.
-    '''
-    async with async_session_maker() as session:
-        try:
-            yield session
-            await session.commit()
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+async def get_session():
+    '''Генератор который отдает нам сессию пока ручка работает'''
+    async with async_session_maker() as session: # создается новая асинхронная сессия
+        yield session # генератор с сессией передается в вызывающую ф-цию
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
-async def get_session_without_commit() -> AsyncGenerator[AsyncSession, None]:
-    '''Асинхронная сессия без автоматического коммита(когда не меняем БД).'''
-    async with async_session_maker() as session:
-        try:
-            yield session
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
